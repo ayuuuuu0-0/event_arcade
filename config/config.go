104faea7ee45006c245ae61e6ae1dfa27a9fa3a9
Ayush Ranjan <ayush.ranjan@omniful.ai>
2026-04-30 17:59:55 +0530

    Added configs

--- config/config.go
+++ config/config.go
@@ -1,1 +1,9 @@
 package config
+
+const (
+	// Bot RNG Probabilities (out of 100)
+	// Powerups are ignored for now, making the idle chance 69%
+	BotChanceHit      = 18
+	BotChanceDodge    = 8
+	BotChanceCritical = 5
+)
--- models/player_actions.go
+++ models/player_actions.go
@@ -5,21 +5,35 @@ import (
 	"time"
 )
 
-type Actions struct {
-	Hit         string `json:"hit"`
-	Dodge       string `json:"dodge"`
-	CriticalHit string `json:"critical_hit"`
-	Idle        string `json:"idle"`
-	ActionId    string `json:"action_id"`
-}
+type Action string
+
+const (
+	ActionHit         Action = "hit"
+	ActionDodge       Action = "dodge"
+	ActionCriticalHit Action = "critical_hit"
+	ActionIdle        Action = "idle"
+)
 
 type Player struct {
 	ID      string        `json:"id"`
-	inputCh chan Actions  // explain me need of this and how channel wil work here
-	timeout time.Duration // explain me need oif this
+	InputCh chan Action   // Channel for receiving real-time actions from a WebSocket (for humans)
+	Timeout time.Duration // Max time the engine will wait for human input before falling back
+	IsBot   bool
+	RNG     *rand.Rand // Seeded random number generator
 }
 
-type Bot struct {
-	ID  string    `json:"id"`
-	RNG rand.Rand `json:"rng"`
+// GetAction is called on every Player every tick. For bots, it returns a random action instantly. For humans, it waits for input on the channel or falls back.
+func (p *Player) GetAction(state MatchState) Action {
+	if p.IsBot {
+		roll := p.RNG.Intn(100)
+
+		return ActionIdle
+	}
+	// For physical players, listen from the websocket channel
+	select {
+	case action := <-p.InputCh:
+		return action
+	case <-time.After(p.Timeout):
+		return ActionIdle
+	}
 }
